e4702d5556e2019863c8961becf65f1baaa89701
C.mirkin <ciromirkin@gmail.com>
2023-05-01 17:26:38 -0300

    add: gets new product form info

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+const addProductForm = document.getElementById('addProductForm')
+addProductForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const newProductNameInput = document.getElementById('newProductName') 
+    const newProductDescriptionInput = document.getElementById('newProductDescription')
+    const newProduct = {
+        name: newProductNameInput.value,
+        description: newProductDescriptionInput.value
+    }
+    
+    newProductNameInput.value = ''
+    newProductDescriptionInput.value = ''
+})
