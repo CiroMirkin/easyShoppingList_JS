3a6644bb565df3a49e1e570e251cc7f68f6f2aab
C.mirkin <ciromirkin@gmail.com>
2023-05-06 09:10:55 -0300

    add: method for show the metrics and the balance

--- src/showBalance.js
+++ src/showBalance.js
@@ -1,3 +1,16 @@
 export default class ShowBalance {
-    
+    showMoneyMetrics(moneyMetrics) {
+        const moneyMetricsElement = document.getElementById('moneyMetrics')
+        moneyMetricsElement.innerHTML = `
+            <li class="list-group-item list-group-item-success d-flex justify-content-between align-items-center">
+            Saldo disponible <span>${moneyMetrics.availableMoney}</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                Dinero a pagar <span>${moneyMetrics.moneyToPay} ARG</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                Dinero sobrante <span>${moneyMetrics.surplus}</span>
+            </li>
+        `
+    }
 }
